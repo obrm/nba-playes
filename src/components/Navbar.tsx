86729cbecf2a2fa0c400582463dfafb9c7ec6917
Ori Baram <obrm770@gmail.com>
2024-04-19 20:18:32 +0300

    change font size of logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,7 +8,7 @@ const Navbar = () => {
       <div className="container mx-auto px-4 sm:px-0">
         <div className="flex-1 flex items-center">
           <img src={logo} alt="Logo" className="h-8 sm:h-12" />
-          <span className="text-lg sm:text-2xl font-bold text-white">NBA Players</span>
+          <span className="text-base sm:text-2xl font-bold text-white">NBA Players</span>
         </div>
         <div className="flex-none">
           <SearchBar />
