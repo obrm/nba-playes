49597d84d75cc5efda2d37e981bda31728dbbded
Ori Baram <obrm770@gmail.com>
2024-04-19 20:23:35 +0300

    fix nav style

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,9 +5,9 @@ import SearchBar from './SearchBar';
 const Navbar = () => {
   return (
     <div className="navbar bg-primary shadow-md">
-      <div className="container mx-auto px-4 sm:px-0">
+      <div className="container mx-auto px-0">
         <div className="flex-1 flex items-center">
-          <img src={logo} alt="Logo" className="h-8 sm:h-12" />
+          <img src={logo} alt="Logo" className="h-8 sm:-ml-3 sm:h-12" />
           <span className="text-base sm:text-2xl font-bold text-white">NBA Players</span>
         </div>
         <div className="flex-none">
