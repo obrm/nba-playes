b655ba9566a2ba29fb996a6a206f76b18a19f73d
Ori Baram <obrm770@gmail.com>
2024-04-19 15:32:29 +0300

    favicon

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,5 +7,11 @@ export default {
 	theme: {
 		extend: {},
 	},
-	plugins: [],
+	plugins: [
+		require('@tailwindcss/typography'),
+		require('daisyui'),
+	],
+	daisyui: {
+		themes: ['winter'],
+	},
 };
