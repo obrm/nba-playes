04602f73d366003fe43d3fa8041405d0eba8dbf5
Ori Baram <obrm770@gmail.com>
2024-04-20 17:43:24 +0300

    show hide search bar

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,12 +1,22 @@
 import { SearchBar, Logo } from '..';
+import { useGlobalFavoriteContext, useIsMobile } from '../../hooks';
 
 const Navbar = () => {
+  const isMobile = useIsMobile();
+  const { showFavorites } = useGlobalFavoriteContext();
+  console.log(isMobile);
+  console.log(showFavorites);
   return (
     <div className="navbar bg-primary shadow-md">
       <div className="container mx-auto px-0">
         <Logo />
         <div className="flex-none">
-          <SearchBar />
+          {!isMobile
+            ? (
+              <SearchBar />
+            ) : isMobile && !showFavorites ? (
+              <SearchBar />
+            ) : null}
         </div>
       </div>
     </div>
