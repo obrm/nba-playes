2e49a797e072c86305430a495eba5775f33a11a0
Ori Baram <obrm770@gmail.com>
2024-04-20 13:52:40 +0300

    refactor FavoritesThemeContext

--- src/context/FavoritesThemeContext.tsx
+++ src/context/FavoritesThemeContext.tsx
@@ -1,27 +1,31 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState, useEffect, useMemo } from 'react';
 
 import { ProviderProps, FavoritesThemeContextType } from '../types/types';
 
 const FavoritesThemeContext = createContext<FavoritesThemeContextType | undefined>(undefined);
 
 export const FavoritesThemeProvider: React.FC<ProviderProps> = ({ children }) => {
-
   const [theme, setTheme] = useState<string>(() => localStorage.getItem('favoritesListTheme') || 'light');
-  const [isDarkTheme, setIsDark] = useState<boolean>(!!JSON.stringify(localStorage.getItem('favoritesListIsDark')) || false);
+  const [isDarkTheme, setIsDarkTheme] = useState<boolean>(() => localStorage.getItem('favoritesListIsDark') === 'true');
+
+  useEffect(() => {
+    localStorage.setItem('favoritesListTheme', theme);
+    localStorage.setItem('favoritesListIsDark', isDarkTheme.toString());
+  }, [theme, isDarkTheme]);
 
   const toggleTheme = () => {
-    const newTheme = isDarkTheme ? 'light' : 'dark';
-    setTheme(newTheme);
-    setIsDark(!isDarkTheme);
-    localStorage.setItem('favoritesListIsDark', isDarkTheme.toString());
-    localStorage.setItem('favoritesListTheme', newTheme);
+    setTheme(isDarkTheme ? 'light' : 'dark');
+    setIsDarkTheme(!isDarkTheme);
   };
 
+  const providerValue = useMemo(() => ({ theme, isDarkTheme, toggleTheme }), [theme, isDarkTheme, toggleTheme]);
+
   return (
-    <FavoritesThemeContext.Provider value={{ theme, isDarkTheme, toggleTheme }}>
+    <FavoritesThemeContext.Provider value={providerValue}>
       {children}
     </FavoritesThemeContext.Provider>
   );
+
 };
 
 export default FavoritesThemeContext;
