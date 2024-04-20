6d7d898cecbd1632b317454aaa13f68a27f795fb
Ori Baram <obrm770@gmail.com>
2024-04-20 16:08:55 +0300

    fix indentation in main

--- src/main.tsx
+++ src/main.tsx
@@ -12,7 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         <FavoriteProvider>
           <App />
         </FavoriteProvider>
-    </PlayersProvider>
+      </PlayersProvider>
     </FavoritesThemeProvider>
   </React.StrictMode>,
 );
